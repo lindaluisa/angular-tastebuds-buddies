6bda958ae260dbab8f08ee48b63ee21839235761
lindaluisa <lindaluisarosenthal@gmail.com>
2018-04-12 23:57:26 +0200

    passing data from dish-list c to dish-item c via property binding

--- src/app/dishes/dishes-list/dishes-item/dishes-item.component.ts
+++ src/app/dishes/dishes-list/dishes-item/dishes-item.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Dish } from '../../dishes.model'; // type Dish
 
 @Component({
   selector: 'app-dishes-item',
   templateUrl: './dishes-item.component.html',
   styleUrls: ['./dishes-item.component.css']
 })
-export class DishesItemComponent implements OnInit {
 
+// Dish not defined; bc dish should come from outside
+export class DishesItemComponent  implements OnInit {
+  @Input() dish: Dish;
   constructor() { }
 
   ngOnInit() {
