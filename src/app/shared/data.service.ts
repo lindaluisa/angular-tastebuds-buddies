6b3dcef0c96aadaf3209d573c899523a35a9add4
lindaluisa <lindaluisarosenthal@gmail.com>
2018-04-25 09:42:18 +0200

    trying to fix deployment issue - http to https

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 import { DishService } from '../dishes/dish.service';
 import { Dish } from '../dishes/dishes.model';
@@ -14,9 +15,20 @@ export class DataService {
 
   getDemDishes() {
     this.http.get('https://tastebuds-buddies.firebaseio.com/dishes.json')
-    .subscribe(
+    .map(
       (response: Response) => {
         const dishes: Dish[] = response.json();
+        for (let dish of dishes) {
+          if (!dish['ingreds']){
+            console.log(dish);
+            dish['ingreds'] = [];
+          }
+        }
+        return dishes;
+      }
+    )
+    .subscribe(
+      (dishes: Dish[]) => {
         this.dishService.setDishes(dishes);
       }
     );
